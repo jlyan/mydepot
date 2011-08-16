b5bd98012310e6cc64a129624cbead6bfb2ab304
jlyan <jinlai.yan@gmail.com>
2011-08-16 10:48:58 +0800

    Validation

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,6 +3,17 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+		    @update = {
+
+      :title       => 'Lorem Ipsum',
+
+      :description => 'Wibbles are fun!',
+
+      :image_url   => 'lorem.jpg',
+
+      :price       => 19.95
+
+    }
   end
 
   test "should get index" do
@@ -18,7 +29,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, :product => @product.attributes
+      post :create, :product => @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +46,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, :id => @product.to_param, :product => @product.attributes
+    put :update, :id => @product.to_param, :product => @update
     assert_redirected_to product_path(assigns(:product))
   end
 
@@ -45,5 +56,141 @@ class ProductsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to products_path
+  end
+
+	  test "product attributes must not be empty" do
+
+    product = Product.new
+
+    assert product.invalid?
+
+    assert product.errors[:title].any?
+
+    assert product.errors[:description].any?
+
+    assert product.errors[:price].any?
+
+    assert product.errors[:image_url].any?
+
+  end
+
+ 
+
+  test "product price must be positive" do
+
+    product = Product.new(:title       => "My Book Title",
+
+                          :description => "yyy",
+
+                          :image_url   => "zzz.jpg")
+
+    product.price = -1
+
+    assert product.invalid?
+
+    assert_equal "must be greater than or equal to 0.01", 
+
+      product.errors[:price].join('; ')
+
+ 
+
+    product.price = 0
+
+    assert product.invalid?
+
+    assert_equal "must be greater than or equal to 0.01", 
+
+      product.errors[:price].join('; ')
+
+ 
+
+    product.price = 1
+
+    assert product.valid?
+
+  end
+
+ 
+
+  def new_product(image_url)
+
+    Product.new(:title       => "My Book Title",
+
+                :description => "yyy",
+
+                :price       => 1,
+
+                :image_url   => image_url)
+
+  end
+
+ 
+
+  test "image url" do
+
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+
+             http://a.b.c/x/y/z/fred.gif }
+
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    
+
+    ok.each do |name|
+
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+
+    end
+
+ 
+
+    bad.each do |name|
+
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+
+    end
+
+  end
+
+ 
+
+  test "product is not valid without a unique title" do
+
+    product = Product.new(:title       => products(:ruby).title,
+
+                          :description => "yyy", 
+
+                          :price       => 1, 
+
+                          :image_url   => "fred.gif")
+
+ 
+
+    assert !product.save
+
+    assert_equal "has already been taken", product.errors[:title].join('; ')
+
+  end
+
+ 
+
+  test "product is not valid without a unique title - i18n" do
+
+    product = Product.new(:title       => products(:ruby).title,
+
+                          :description => "yyy", 
+
+                          :price       => 1, 
+
+                          :image_url   => "fred.gif")
+
+ 
+
+    assert !product.save
+
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+
+                 product.errors[:title].join('; ')
+
   end
 end
