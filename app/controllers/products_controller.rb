6bf9293909e7d737f9e38e8aedd2c0a9e0a337ab
jlyan <jinlai.yan@gmail.com>
2011-08-17 15:34:23 +0800

    Orders

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -80,4 +80,16 @@ class ProductsController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  def who_bought
+  	@product = Product.find(params[:id])
+  	respond_to do |format|
+  		format.html
+  		format.xml 
+  		format.xml
+  		format.atom
+  		format.json { render :json => @product.to_json(:include => :orders) }
+  	end
+  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
 	 
 	has_many :line_items
+	
+	has_many :orders, :through => :line_items
 
 	before_destroy :ensure_not_referenced_by_any_line_item 
  validates :title, :description, :image_url, :presence => true
