7c4e5900e7aae658c3bf6dbd454c020fb0eb83d2
jlyan <jinlai.yan@gmail.com>
2011-08-17 16:43:31 +0800

    formats

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,23 @@ Depot::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  
+  #Don't actually send emails
+  config.action_mailer.delivery_method = :test
+  
+  # Alternate configuration example, using gmail:
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #	    address:        "smtp.gmail.com",
+  #	    port:           587, 
+  #	    domain:         "domain.of.sender.net",
+  #	    authentication: "plain",
+  #	    user_name:      "dave",
+  #	    password:       "secret",
+  #     enable_starttls_auto: true
+  #	  } 
+  
+  
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
