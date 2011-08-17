0dfe64a48ad804132ee079191ac7acf54f3513ba
jlyan <jinlai.yan@gmail.com>
2011-08-17 20:05:35 +0800

    Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize
+  
+  
   protect_from_forgery
 
 	private
@@ -10,5 +13,17 @@ class ApplicationController < ActionController::Base
 		session[:cart_id] = cart.id
 		cart
 	end	
-
+	
+	protected
+	
+	def authorize
+		if request.format == Mime::HTML
+			unless User.find_by_id(session[:user_id])
+				redirect_to login_url, :notice => "Please log in"
+			end
+		else
+			authenticate_or_request_with_http_basic do |username, password|
+				user = User.authenticate(username, password)
+		end
+	end
 end
